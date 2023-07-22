6322164f8dbdb196026f17860d2b5ebba7034fae
Roozbeh Haghighi <rhaghighi3@gatech.edu>
2023-07-22 18:26:30 +0200

    first commit

---
+++ READMe.md
@@ -0,0 +1,11 @@
+MS QCF Workshop Fall 2023
+## Overview
+
+This is an example repo for MS QCF workshop
+
+## Contents
+
+An implementation of absolute value function
+
+
+# my_abs_project
---
+++ my_abs.py
@@ -0,0 +1,19 @@
+import math
+import numbers
+
+def my_abs(x):
+    if isinstance(x, numbers.Real):
+        if x < 0:
+            return -x
+        else:
+            return x
+    elif isinstance(x, numbers.Complex):
+        return math.sqrt(x.real ** 2 + x.imag ** 2)
+    else:
+        return math.nan
+    
+def my_almost_eq(x, y):
+    if my_abs(x -y ) < 1e-16:
+        return True
+    else:
+        return False
